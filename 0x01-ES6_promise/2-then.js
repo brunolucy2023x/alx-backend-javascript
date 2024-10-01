d811f7a682143ed15f1bc96a4e61d7928642ca8d
brunox327 <lucyb5147@gmail.com>
2024-10-01 23:37:22 +0000

    trial two

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,3 @@
-// Bruno Okoth
-// This function handles a promise from an API. It returns an object
-// with a success status and message if the promise resolves, 
-// creates an Error object if it rejects, and logs a message when 
-// the promise is settled (either resolved or rejected).
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
