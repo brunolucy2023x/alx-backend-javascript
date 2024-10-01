d099c2e04dfdc1469c4af15eab8a8f36c69962ad
brunox327 <lucyb5147@gmail.com>
2024-10-01 23:42:40 +0000

    trial 4

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,7 @@
-// Bruno Okoth
-// This function handles profile signup by uploading a photo and creating a user.
-// It waits for both operations to complete using Promise.all. If successful,
-// it logs the response body and the user's full name. If either operation fails,
-// it logs an error message.
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([body, fullName]) => 
-      console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
