bf4fc37c999328a50c395104f6dda097dfe6851d
brunox327 <lucyb5147@gmail.com>
2024-10-03 23:40:21 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,12 @@
+// Importing the ClassRoom class from the specified module
 import ClassRoom from './0-classroom';
 
+// Function to initialize an array of classroom instances with different maximum sizes
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  // Creating and returning an array with new ClassRoom instances
+  return [
+    new ClassRoom(19), // Classroom with a maximum size of 19 students
+    new ClassRoom(20), // Classroom with a maximum size of 20 students
+    new ClassRoom(34)  // Classroom with a maximum size of 34 students
+  ];
 }
