3a096269c2c9499a9dd2c8454bf65b76d453f08d
brunox327 <lucyb5147@gmail.com>
2024-10-03 23:34:56 +0000

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,8 @@
+// Exporting the ClassRoom class as the default export
 export default class ClassRoom {
+  // Constructor to initialize the maximum student size
   constructor(maxStudentSize) {
+    // Setting the maximum number of students allowed in the classroom
     this._maxStudentsSize = maxStudentSize;
   }
 }
